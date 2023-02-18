14c29b4469c381028431f72d7f6599e879adc871
kelvinkn17 <kelvin.kn17@gmail.com>
2023-02-19 04:40:10 +0700

    first commit

---
+++ types/components/ButtonSelectList.d.ts
@@ -0,0 +1,9 @@
+export declare const ButtonSelectList: <T extends Object>({ options, value, onChange, group, }: {
+    options: {
+        value: T;
+        text: string;
+    }[];
+    value: T | null;
+    onChange: (value: T) => void;
+    group: string;
+}) => JSX.Element;
